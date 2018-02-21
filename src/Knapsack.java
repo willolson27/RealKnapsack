fc1a48e8d1351d436c65d9bdfe3148e507ebc91a
OlsonW <OlsonW@104-07-2017.bi.local>
2018-02-21 09:50:30 -0500

    two

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -1,4 +1,42 @@
+import java.util.List;
 
 public class Knapsack {
 
+	public static int knapsackSum(int[] w, int n, int limit) {
+		if (w[n] == 0)
+			return 0;
+		if (w[n] > limit)
+			return 0;
+		int withLast = knapsackSum(w, n, limit - w[n]);
+		int withoutLast = knapsackSum(w, n-1, limit);
+		if (withLast > withoutLast)
+			return withLast;
+		else
+			return withoutLast;
+		
+		
+		
+	}
+	
+	
+	public static int knapsackSum(int[] w, int n, int limit, List<Integer> list) {
+		if (w[n] == 0)
+			return 0;
+		if (w[n] > limit)
+			return 0;
+		list.add(w[n]);
+		int withLast = knapsackSum(w, n, limit - w[n]);
+		int withoutLast = knapsackSum(w, n-1, limit);
+		if (withLast > withoutLast)
+			return withLast;
+		else
+			return withoutLast;
+		
+		
+		
+	}
+	
+	
+	
+	
 }
