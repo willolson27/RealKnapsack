c7863a6385737bc115fa546190def7a26c6bd8fc
Bob <Bob@DESKTOP-GVDICQ4.fios-router.home>
2018-02-22 09:55:26 -0500

    three

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -1,6 +1,11 @@
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class Knapsack {
+	
+	
+	private static final String ERROR = "Error: File not found";
+	private static final String PROMPT = "Please provide a file name";
 
 	public static int knapsackSum(int[] w, int n, int limit) {
 		if (w[n] == 0)
@@ -32,6 +37,46 @@ public class Knapsack {
 		else
 			return withoutLast;
 		
+		
+		
+	}
+	
+	
+	public static ArrayList<String> readFile(String filename) throws IOException {
+		
+		ArrayList<String> fileNames = new ArrayList<String>();
+		
+		BufferedReader inputReader = null;
+	    
+	    try {
+	    	inputReader = new BufferedReader(new FileReader(filename), 1024);
+	    }
+	    catch (FileNotFoundException e) {
+	    	System.out.println(ERROR);
+	    	System.exit(0);
+	    }
+	    	
+	   	String line;
+	    while ((line = inputReader.readLine()) != null)
+	      fileNames.add(line);
+	    	
+		return fileNames;
+		
+		
+	}
+	
+	public static void main(String[] args) throws IOException {
+		
+		ArrayList<String> fileNames;
+		Scanner keyboard = new Scanner(System.in);
+		if (args[0] != null)
+			fileNames = readFile(args[0]);
+		else {
+			System.out.println(PROMPT);
+			fileNames = readFile(keyboard.nextLine());
+		}
+		for (String f : fileNames)
+			System.out.println(f);
 		
 		
 	}
